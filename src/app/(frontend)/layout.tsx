b718a5619752c93be6d41bb17221cd92e3d23723
jaedalzaber <jaedalzaber@gmail.com>
2025-07-25 11:45:32 +0600

    logo test

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -13,8 +13,9 @@ import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import { draftMode } from 'next/headers'
 
-import './globals.css'
 import { getServerSideURL } from '@/utilities/getURL'
+import './globals.css'
+import SmoothScrollProvider from '@/components/SmoothScrollProvider'
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const { isEnabled } = await draftMode()
@@ -34,9 +35,11 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             }}
           />
 
-          <Header />
-          {children}
-          <Footer />
+          <SmoothScrollProvider>
+            <Header />
+            {children}
+            <Footer />
+          </SmoothScrollProvider>
         </Providers>
       </body>
     </html>
